ac380e43212e7f268770643046dbec4daabef995
AstralRomance <andrdoncwork@mail.ru>
2019-09-24 10:53:55 +0300

    Delete affin_classification.py

--- affin_classification.py
+++
@@ -1,9 +0,0 @@
-from sklearn.cluster import AffinityPropagation
-import numpy as np
-import xls_to_csv_parser
-import matplotlib.pyplot as plt
-from itertools import cycle
-
-data = xls_to_csv_parser.parser()
-
-classifier = AffinityPropagation().fit(data)
