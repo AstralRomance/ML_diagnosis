14113f240c6267410466f85c81e8dd07c613c4ba
AstralRomance <andrdoncwork@mail.ru>
2019-10-08 07:20:17 +0300

    Add files via upload

--- Analyzer.py
+++ Analyzer.py
@@ -4,7 +4,9 @@ from matplotlib import pyplot as plt
 class Analyzer:
     def __init__(self, cluster_labels, data=None):
         self.points_per_cluster = Counter(cluster_labels)
+        self.points_cluster_dict = data
         self.prob_per_cluster = []
+        self.avg_cluster_probability = []
 
 #   Probability calculating for clusters
     def probability_calc(self):
@@ -12,6 +14,24 @@ class Analyzer:
         for i in self.points_per_cluster.keys():
             self.prob_per_cluster.append((self.points_per_cluster.get(i)/total_points)*100)
         self.__probability_print()
+
+#   Counting math expectation for each component per clusters
+#   Return list with expectation for each component per cluster
+    def math_expectation(self):
+        temp = 0
+        math_expectation = []
+        for clusters in self.points_cluster_dict.keys():
+            temp_exp = []
+            for x_component in self.points_cluster_dict[clusters][0]:
+                temp += x_component
+            temp_exp.append(temp/len(self.points_cluster_dict[clusters]))
+            temp = 0
+            for y_component in self.points_cluster_dict[clusters][1]:
+                temp += y_component
+            temp_exp.append(temp/len(self.points_cluster_dict[clusters]))
+            math_expectation.append(math_expectation)
+            temp_exp.clear()
+        return math_expectation
 
 #   Closed method to print probability per cluster
     def __probability_print(self):
